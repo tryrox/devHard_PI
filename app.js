249fe920d80756cd844ff096df23987ba874c3a8
Rafael Yudi <rafael_yudi07@hotmail.com>
2022-07-20 15:16:04 -0400

    organizando branchs

--- app.js
+++ app.js
@@ -13,17 +13,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'src' ,'views'));
 app.set('view engine', 'ejs');
-app.get('/home', function(req, res){
-res.render('home')
-});
-
-app.get('/', function (req, res){
-  res.json({name: 'gustavo', idade:20})
-});
-
-app.listen(3000, function(){
-
-})
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/produtos', produtosRouter);
+app.use('/carrinho', carrinhoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
